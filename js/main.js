0e9c26ae56fafaf1881c0cf400069885788b0140
20Federico <fefe.molino@gmail.com>
2021-11-02 17:02:09 +0100

    html-css structure

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+Vue.config.devtools = true;
+
+window.addEventListener('DOMContentLoaded', function () {
+  const vueApp = new Vue({
+      el: '',
+      data: {
+        username: '',
+      },
+      methods: {
+        function () {
+          
+        },
+      },
+  });
+
+
+})
